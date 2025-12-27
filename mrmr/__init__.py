d3266088baf1e04a385b4b3df59be8547fa53435
kmedved <bibigon@gmail.com>
2025-12-27 23:08:36 +0800

    Update package exports

--- mrmr/__init__.py
+++ mrmr/__init__.py
@@ -1,8 +1,23 @@
-from . import bigquery
-from . import pandas
-from . import polars
-from . import spark
-from .pandas import mrmr_classif, mrmr_regression
-from .main import mrmr_base
+from .pandas import (
+    mrmr_classif,
+    mrmr_regression,
+    jmi_classif,
+    jmi_regression,
+    jmim_classif,
+    jmim_regression,
+    cefsplus_select,
+)
+from .main import mrmr_base, jmi_base
+from .fast_mi import (
+    regression_joint_mi,
+    binned_joint_mi,
+    ksg_joint_mi,
+)
+from .cefsplus import (
+    cefsplus_regression,
+    build_cache,
+    select_features_cached,
+    FeatureCache,
+)
 
-__version__ = "0.2.9"
+__version__ = "0.3.0"
